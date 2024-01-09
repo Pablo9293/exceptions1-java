affb14fd856da7139a81dd081f5d9d851c5d9169
Pablo Cupertino <pabloagm7@gmail.com>
2024-01-08 22:14:01 -0300

    solucao ruim

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -18,32 +18,27 @@ public class Programa {
 		Date dataDeEntrada = sdf.parse(sc.next()); // Entra com a data no formato de texto, e o SimpleDateFormat formata
 		System.out.print("Data de Saida (dd/MM/yyyy):");
 		Date dataDeSaida = sdf.parse(sc.next());
-		if (!dataDeSaida.after(dataDeEntrada)) {
-			System.out.println("Erro na reserva:data de saida tem que ser depois da data de entrada!!!");
+
+		Reserva reserva = new Reserva(numero, dataDeEntrada, dataDeSaida);
+		System.out.println("Reserva: " + reserva);
+		System.out.println();
+		System.out.println("Atualize a data de reserva: ");
+		System.out.print("Data de Entrada (dd/MM/yyyy):");
+		dataDeEntrada = sdf.parse(sc.next()); // Entra com a data no formato de texto, e o SimpleDateFormat formata
+		System.out.print("Data de Saida (dd/MM/yyyy):");
+		dataDeSaida = sdf.parse(sc.next());
+
+		String error = reserva.atualizaDatas(dataDeEntrada, dataDeSaida);
+		if (error != null) {
+			System.out.println("Erro na reserva: " + error);
 		} else {
-			Reserva reserva = new Reserva(numero, dataDeEntrada, dataDeSaida);
 			System.out.println("Reserva: " + reserva);
-			System.out.println();
-			System.out.println("Atualize a data de reserva: ");
-			System.out.print("Data de Entrada (dd/MM/yyyy):");
-			dataDeEntrada = sdf.parse(sc.next()); // Entra com a data no formato de texto, e o SimpleDateFormat formata
-			System.out.print("Data de Saida (dd/MM/yyyy):");
-			dataDeSaida = sdf.parse(sc.next());
-			Date agora = new Date(); // Cria uma data deste momento
-			if (dataDeEntrada.before(agora) || dataDeSaida.before(agora)) {
-				System.out.print("Erro na reserva: As datas para atualizacao tem que ser datas futuras!!!");
-			} else if (!dataDeSaida.after(dataDeEntrada)) {
-				System.out.println("Erro na reserva:data de saida tem que ser depois da data de entrada!!!");
-			} else {
-
-				reserva.atualizaDatas(dataDeEntrada, dataDeSaida);
-				System.out.println("Reserva: " + reserva);
-			}
-
 		}
 
-		sc.close(); // Problemas esta lógica esta no programa principal,o ideal seria ela estar na reserva
+		// }
+
+		sc.close(); // Problemas esta lógica esta no programa principal,o ideal seria ela estar na
+					// reserva
 
 	}
-
 }
--- src/model/entities/Reserva.java
+++ src/model/entities/Reserva.java
@@ -33,9 +33,17 @@ public long duracao() {
 	long dif = dataDeSaida.getTime() - dataDeEntrada.getTime();
 	return TimeUnit.DAYS.convert(dif, TimeUnit.MILLISECONDS);
 }
-public void atualizaDatas(Date dataDeEntrada , Date dataDeSaida) {
-	this.dataDeEntrada = dataDeEntrada;
+public String atualizaDatas(Date dataDeEntrada , Date dataDeSaida) {
+	Date agora = new Date(); // Cria uma data deste momento
+	if (dataDeEntrada.before(agora) || dataDeSaida.before(agora)) {
+		return "Erro na reserva: As datas para atualizacao tem que ser datas futuras!!!";
+	}
+	if (!dataDeSaida.after(dataDeEntrada)) {
+		return "Erro na reserva:data de saida tem que ser depois da data de entrada!!!";
+	}
+	this.dataDeEntrada = dataDeEntrada; // Passando pelos if sem erro executa as atualizaçoes de entrada e saida
 	this.dataDeSaida = dataDeSaida;
+	return null; // Não deu erro
 }
 @Override // Pois o toString e uma sobreposicao
 public String toString() {
